86d3ace39c008af66157fd813708b311e302367c
Genc Bytyci <genc.bytyci@bulme.at>
2021-10-06 10:37:56 +0200

    Interacitvity

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,13 @@
 // mein kaka programm
 #include <iostream>
+using namespace std;
 
 int main()
-{
-	std::cout << "Hallo 4BHEL!" << std::endl;
+{	
+	string name;
+	cout << "Hallo 4BHEL!" << endl;
+	cout << "Dein Name: ";
+	cin >> name; 
+	cout << "Hallo " << name << endl;
 	return 0;
 }
